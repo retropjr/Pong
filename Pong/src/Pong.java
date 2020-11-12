c6f4e6904118e0912012e66cc40e678250a160e7
Richard Porter <retropjr@hotmail.com>
2020-11-12 15:17:33 +1300

    JPanel exercise completed

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,5 +1,12 @@
 import javax.swing.JFrame;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
 public class Pong extends JFrame {
 	
 	/*Update code to use final static variables for the window title, 
@@ -11,7 +18,7 @@ public class Pong extends JFrame {
 	private static final int WINDOW_WIDTH = 800; 
 	private static final int WINDOW_HEIGHT = 600;
 	private static final String WINDOW_TITLE = "Pong";
-	
+	private static final Color BACKGROUND_COLOR = Color.BLACK;
 	
 	public Pong() {
     	setTitle(WINDOW_TITLE);
@@ -19,10 +26,44 @@ public class Pong extends JFrame {
     	setResizable(false);
     	setVisible(true);
     	setDefaultCloseOperation(EXIT_ON_CLOSE);
+    	
+    	add(new PongPanel());
 	}
+	
+	public class PongPanel extends JPanel implements ActionListener, KeyListener {
+		
+		public PongPanel() {
+	          setBackground(BACKGROUND_COLOR);
+	      }
+		
+		@Override
+		public void keyTyped(KeyEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		  
+	 }
 
 	public static void main(String[] args) {
 		new Pong();
-
+		
 	}
 }
