4b8d36ad80d0055da42adc652bc9c1bbe9048dc9
Richard Porter <retropjr@hotmail.com>
2020-11-12 14:38:13 +1300

    Up to end of JFrame exercise, includes static variables.

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,10 +1,28 @@
+import javax.swing.JFrame;
 
-public class Pong {
-
-	public static void main(String[] args) {
-		// Test compile
-		System.out.println("Test compile and print");
-
+public class Pong extends JFrame {
+	
+	/*Update code to use final static variables for the window title, 
+	 *width, and height. Set the width to 800, the height to 600, and the title to “Pong”. 
+	 *Replace the values in the method calls in the constructor with these variables. 
+	 *The variables are all upper case with a descriptive name.
+	*/
+	
+	private static final int WINDOW_WIDTH = 800; 
+	private static final int WINDOW_HEIGHT = 600;
+	private static final String WINDOW_TITLE = "Pong";
+	
+	
+	public Pong() {
+    	setTitle(WINDOW_TITLE);
+    	setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    	setResizable(false);
+    	setVisible(true);
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
+	public static void main(String[] args) {
+		new Pong();
+
+	}
 }
