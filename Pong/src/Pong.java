c0b8b40c0b7b2422cf4c3946b68f82409a6e1059
Richard Porter <retropjr@hotmail.com>
2020-11-13 16:06:56 +1300

    After addition of paintComponent()

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Pong extends JFrame {
 	
@@ -18,7 +21,7 @@ public class Pong extends JFrame {
 	private static final int WINDOW_WIDTH = 800; 
 	private static final int WINDOW_HEIGHT = 600;
 	private static final String WINDOW_TITLE = "Pong";
-	private static final Color BACKGROUND_COLOR = Color.BLACK;
+	
 	
 	public Pong() {
     	setTitle(WINDOW_TITLE);
@@ -30,10 +33,16 @@ public class Pong extends JFrame {
     	add(new PongPanel());
 	}
 	
-	public class PongPanel extends JPanel implements ActionListener, KeyListener {
+	public static class PongPanel extends JPanel implements ActionListener, KeyListener {
+		
+		private final static Color BACKGROUND_COLOUR = Color.BLACK;
+		private final static int TIMER_DELAY = 5;
 		
 		public PongPanel() {
-	          setBackground(BACKGROUND_COLOR);
+	         
+	          setBackground(BACKGROUND_COLOUR);
+	          Timer timer = new Timer(TIMER_DELAY, this);
+	             timer.start();
 	      }
 		
 		@Override
@@ -53,12 +62,35 @@ public class Pong extends JFrame {
 			// TODO Auto-generated method stub
 			
 		}
+		
+		@Override
+		 public void paintComponent(Graphics g) {
+		     super.paintComponent(g);
+		     g.setColor(Color.WHITE);
+		     g.fillRect(20, 20, 100, 100);
+		 }
 
+		
+		
+		/*
+		 * This will be your first time seeing the @Override keyword. 
+		 * The actionPerformed() method belongs to the ActionListener class and 
+		 * we want to customise the behaviour for our own purpose. We override the default 
+		 * behaviour of the actionPerformed() method – we use the @Override keyword to identify that 
+		 * we are doing this. Whenever we override a method from a parent class it is good practice 
+		 * to use @Override; this will make the compiler check that we are correctly overriding an 
+		 * existing method – and then we will receive a warning if we haven’t.
+		 */
+		
 		@Override
 		public void actionPerformed(ActionEvent event) {
-			// TODO Auto-generated method stub
 			
+			update();
 		}
+		
+		private void update() {
+	          
+		 }
 		  
 	 }
 
