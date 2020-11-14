ebc6f2c444cb189fd4160602820694b8056cb409
Richard Porter <retropjr@hotmail.com>
2020-11-14 19:09:57 +1300

    Ball movement, Wall and Paddle collisions implemented

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -20,7 +20,7 @@ public class Pong extends JFrame {
 	private static final int WINDOW_WIDTH = 800; 
 	private static final int WINDOW_HEIGHT = 600;
 	private static final String WINDOW_TITLE = "Pong";
-	
+	private final static int BALL_MOVEMENT_SPEED = 2;
 	
 	 
 	public Pong() {
@@ -48,6 +48,8 @@ public class Pong extends JFrame {
 	          setBackground(BACKGROUND_COLOUR);
 	          Timer timer = new Timer(TIMER_DELAY, this);
 	          timer.start();
+	          addKeyListener(this);
+	          setFocusable(true);
 	      }
 		
 		public void createObjects() {
@@ -65,14 +67,26 @@ public class Pong extends JFrame {
 
 		@Override
 		public void keyPressed(KeyEvent event) {
-			// TODO Auto-generated method stub
-			
+			if(event.getKeyCode() == KeyEvent.VK_W) {
+				paddle1.setYVelocity(-1);
+			} else if(event.getKeyCode() == KeyEvent.VK_S) {
+				paddle1.setYVelocity(1);
+			}
+			if(event.getKeyCode() == KeyEvent.VK_UP) {
+				paddle2.setYVelocity(-1);
+			} else if(event.getKeyCode() == KeyEvent.VK_DOWN) {
+				paddle2.setYVelocity(1);
+			}
 		}
 
 		@Override
 		public void keyReleased(KeyEvent event) {
-			// TODO Auto-generated method stub
-			
+			if(event.getKeyCode() == KeyEvent.VK_W || event.getKeyCode() == KeyEvent.VK_S) {
+				paddle1.setYVelocity(0);
+			}
+			if(event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_DOWN) {
+				paddle2.setYVelocity(0);
+			}
 		}
 		
 				
@@ -122,20 +136,63 @@ public class Pong extends JFrame {
 		
 		
 		private void update() {
-	           switch(gameState) {
-	               case Initialising: {
-	                   createObjects();
-	                  gameState = GameState.Playing;
-	                   break;
-	               }
-	               case Playing: {
-	                   break;
-	              }
-	              case GameOver: {
-	                  break;
-	              }
-	          }
-	      }
+		       switch(gameState) {
+		           case Initialising: {
+		               createObjects();
+		               gameState = GameState.Playing;
+		               ball.setXVelocity(BALL_MOVEMENT_SPEED);
+		               ball.setYVelocity(BALL_MOVEMENT_SPEED);
+		               break;
+		          }
+		          case Playing: {
+		              moveObject(paddle1);
+		              moveObject(paddle2);
+		        	  moveObject(ball);         
+		              checkWallBounce();
+		              checkPaddleBounce();
+		              break;
+		          }
+		          case GameOver: {
+		              break;
+		          }
+		      }
+		 }
+		
+		
+		private void moveObject(Sprite object) {
+		      object.setXPosition(object.getXPosition() + object.getXVelocity(),getWidth());
+		      object.setYPosition(object.getYPosition() + object.getYVelocity(),getHeight());
+		 }
+		
+				
+		
+		private void checkWallBounce() {
+		      if(ball.getXPosition() <= 0) {
+		          // Hit left side of screen
+		    	  resetBall();
+		      } else if(ball.getXPosition() >= getWidth() - ball.getWidth()) {
+		          // Hit right side of screen
+		    	  resetBall();
+		      }
+		      if(ball.getYPosition() <= 0 || ball.getYPosition() >= getHeight() - ball.getHeight()) {
+		          // Hit top or bottom of screen
+		          ball.setYVelocity(-ball.getYVelocity());
+		      }
+		 }
+		
+		private void resetBall() {
+			ball.resetToInitialPosition();
+		}
+		
+		private void checkPaddleBounce() {
+		      if(ball.getXVelocity() < 0 && ball.getRectangle().intersects(paddle1.getRectangle())) {
+		          ball.setXVelocity(BALL_MOVEMENT_SPEED);
+		      }
+		      if(ball.getXVelocity() > 0 && ball.getRectangle().intersects(paddle2.getRectangle())) {
+		          ball.setXVelocity(-BALL_MOVEMENT_SPEED);
+		      }
+		}
+
 	}
 	
 	/*
@@ -221,7 +278,7 @@ public class Pong extends JFrame {
 		}
 		
 		public void setYVelocity(int newYVelocity) {
-			xVelocity = newYVelocity;
+			yVelocity = newYVelocity;
 		}
 		
 		
